b50f38f84be61dee6f8bb57bc5a657032a4b5ef4
rtxsc <robotronix.uitm@gmail.com>
2021-10-12 22:57:51 +0800

    update

--- connect2wifi.py
+++ connect2wifi.py
@@ -1,9 +1,13 @@
+#edit 12 Oct
 import board
 import digitalio
 import busio as io
 import subprocess
-import socket
 import adafruit_ssd1306
+import netifaces
+import json
+import socket
+
 
 from time import sleep
 
@@ -17,31 +21,49 @@ from digitalio import DigitalInOut
 reset_pin = DigitalInOut(board.D21) # any pin!
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
 
-oled.fill(1)
-oled.show()
-sleep(0.5)
-buzz.value = True
-sleep(0.05)
-buzz.value = False
 
-oled.fill(0)
-for i in range(32,96):
-    oled.pixel(i, 0, 1)
-    oled.pixel(i, 1, 1)
-    oled.show()
-oled.fill(0)
-oled.text('Hello from Pi3B+', 0, 0, True)
-oled.show()
-sleep(1)
+# for i in range(0,32):
+#     oled.pixel(i, 0, 1)
+#     oled.pixel(i, 1, 1)
+#     oled.show()
+# oled.fill(0)
 
 try:
+    oled.fill(1)
+    oled.show()
+    sleep(0.5)
+    oled.fill(0)
+    buzz.value = True
+    sleep(0.05)
+    buzz.value = False
+    sleep(0.5)
+
+    buzz.value = True
+    sleep(0.05)
+    buzz.value = False
+    sleep(0.5)
+
+    oled.text('Hello from Pi3B+', 0, 0, True)
+    oled.show()
+    sleep(0.5)
     print("checking for SSID")
     output = subprocess.check_output(['iwgetid'])
+
+    device = 'wlan0'
+    cmd = "ip addr show %s | awk '$1 == \"inet\" {gsub(/\/.*$/, \"\", $2); print $2}'" % device
+
+    p = subprocess.Popen(cmd, shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    out, _ = p.communicate()
+    local_ip = str(out.strip().decode('UTF-8'))
+    print(local_ip)
+
+    device = 'wlan0'
+    dict = netifaces.ifaddresses(device)[netifaces.AF_INET]
+    print(dict)
+
     print("connected!")
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    local_ip = s.getsockname()[0]
-    print("IP Address:{}".format(local_ip))
 
     oled.fill(0)
     oled.text(local_ip, 0, 0, True)
